e1aec7e4e8e41f546c59bbf1da164773ec8731df
scalahub <23208922+scalahub@users.noreply.github.com>
2022-11-16 17:57:05 +0530

    Dexy: Test for invalid LP swaps (wrong amount)

--- kiosk/src/test/scala/kiosk/dexy/LpSpec.scala
+++ kiosk/src/test/scala/kiosk/dexy/LpSpec.scala
@@ -25,7 +25,7 @@ class LpSpec  extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks
   val fakeIndex = 1.toShort
   val changeAddress = "9gQqZyxyjAptMbfW1Gydm3qaap11zd6X9DrABwgEE9eRdRvd27p"
 
-  property("Swap (sell Ergs should work)") {
+  property("Swap (sell Ergs) should work") {
     val lpInCirc = 10000L
     val oracleRateXy = 10000L
     val lpBalance = 100000000L
@@ -44,10 +44,6 @@ class LpSpec  extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks
     val deltaReservesY = reservesYOut - reservesYIn
 
     assert(BigInt(deltaReservesY) * reservesXIn * feeDenomLp >= BigInt(deltaReservesX) * (feeNumLp - feeDenomLp) * reservesYIn )
-
-    val dummyInt = 102
-    val dummyToken = "472B4B6250655368566D597133743677397A24432646294A404D635166546A11"
-    val dummyTokenAmount = 110
 
     ergoClient.execute { implicit ctx: BlockchainContext =>
 
@@ -119,7 +115,186 @@ class LpSpec  extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks
     }
   }
 
-  property("Swap (sell Dexy should work)") {
+  property("Swap (sell Ergs) should work irrespective of oracle rate") {
+    val lpInCirc = 10000L
+    val oracleRateXy = 0L // set oracle rate to 0
+    val lpBalance = 100000000L
+    val reservesXIn = 1000000000000L
+    val reservesYIn = 100000000L
+
+    val rate = reservesYIn.toDouble / reservesXIn
+    val sellX = 10000000L
+    val buyY = (sellX * rate * (feeDenomLp - feeNumLp) / feeDenomLp).toLong
+    assert(buyY == 997)
+
+    val reservesXOut = reservesXIn + sellX
+    val reservesYOut = reservesYIn - buyY
+
+    val deltaReservesX = reservesXOut - reservesXIn
+    val deltaReservesY = reservesYOut - reservesYIn
+
+    assert(BigInt(deltaReservesY) * reservesXIn * feeDenomLp >= BigInt(deltaReservesX) * (feeNumLp - feeDenomLp) * reservesYIn)
+
+    ergoClient.execute { implicit ctx: BlockchainContext =>
+
+      val fundingBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(fakeNanoErgs)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId1, fakeIndex)
+
+      val dummyBox = // ToDo: see if this can be removed
+        ctx
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(dummyNanoErgs)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId5, fakeIndex)
+
+      val oracleBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(minStorageRent)
+          .tokens(new ErgoToken(oracleNFT, 1))
+          .registers(KioskLong(oracleRateXy).getErgoValue)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId2, fakeIndex)
+
+      val lpBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(reservesXIn)
+          .tokens(new ErgoToken(lpNFT, 1), new ErgoToken(lpToken, lpBalance), new ErgoToken(dexyUSD, reservesYIn))
+          .registers(KioskLong(lpInCirc).getErgoValue)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), lpScript))
+          .build()
+          .convertToInputWith(fakeTxId3, fakeIndex)
+
+      val validLpOutBox = KioskBox(
+        lpAddress,
+        reservesXOut.toLong,
+        registers = Array(KioskLong(lpInCirc)),
+        tokens = Array((lpNFT, 1), (lpToken, lpBalance), (dexyUSD, reservesYOut.toLong))
+      )
+
+      val dummyOutputBox = KioskBox(
+        changeAddress,
+        dummyNanoErgs,
+        registers = Array(),
+        tokens = Array((dexyUSD, buyY.toLong))
+      )
+
+      // all ok, swap should work
+      noException shouldBe thrownBy {
+        TxUtil.createTx(Array(lpBox, dummyBox, fundingBox), Array(oracleBox),
+          Array(validLpOutBox, dummyOutputBox),
+          fee = 1000000L,
+          changeAddress,
+          Array[String](),
+          Array[DhtData](),
+          false
+        )
+      }
+    }
+  }
+
+  property("Swap (sell Ergs) should not work if more Dexy taken") {
+    val lpInCirc = 10000L
+    val oracleRateXy = 10000L
+    val lpBalance = 100000000L
+    val reservesXIn = 1000000000000L
+    val reservesYIn = 100000000L
+
+    val rate = reservesYIn.toDouble / reservesXIn
+    val sellX = 10000000L
+    val buyY = (sellX * rate * (feeDenomLp - feeNumLp) / feeDenomLp).toLong + 1 // taken 1 more dexy token than allowed
+    assert(buyY == 998)
+
+    val reservesXOut = reservesXIn + sellX
+    val reservesYOut = reservesYIn - buyY
+
+    val deltaReservesX = reservesXOut - reservesXIn
+    val deltaReservesY = reservesYOut - reservesYIn
+
+    assert(BigInt(deltaReservesY) * reservesXIn * feeDenomLp < BigInt(deltaReservesX) * (feeNumLp - feeDenomLp) * reservesYIn )
+
+    ergoClient.execute { implicit ctx: BlockchainContext =>
+
+      val fundingBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(fakeNanoErgs)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId1, fakeIndex)
+
+      val dummyBox = // ToDo: see if this can be removed
+        ctx
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(dummyNanoErgs)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId5, fakeIndex)
+
+      val oracleBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(minStorageRent)
+          .tokens(new ErgoToken(oracleNFT, 1))
+          .registers(KioskLong(oracleRateXy).getErgoValue)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId2, fakeIndex)
+
+      val lpBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(reservesXIn)
+          .tokens(new ErgoToken(lpNFT, 1), new ErgoToken(lpToken, lpBalance), new ErgoToken(dexyUSD, reservesYIn))
+          .registers(KioskLong(lpInCirc).getErgoValue)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), lpScript))
+          .build()
+          .convertToInputWith(fakeTxId3, fakeIndex)
+
+      val validLpOutBox = KioskBox(
+        lpAddress,
+        reservesXOut.toLong,
+        registers = Array(KioskLong(lpInCirc)),
+        tokens = Array((lpNFT, 1), (lpToken, lpBalance), (dexyUSD, reservesYOut.toLong))
+      )
+
+      val dummyOutputBox = KioskBox(
+        changeAddress,
+        dummyNanoErgs,
+        registers = Array(),
+        tokens = Array((dexyUSD, buyY.toLong))
+      )
+
+      an[Exception] shouldBe thrownBy {
+        TxUtil.createTx(Array(lpBox, dummyBox, fundingBox), Array(oracleBox),
+          Array(validLpOutBox, dummyOutputBox),
+          fee = 1000000L,
+          changeAddress,
+          Array[String](),
+          Array[DhtData](),
+          false
+        )
+      }
+    }
+  }
+
+  property("Swap (sell Dexy) should work") {
     val lpInCirc = 10000L
     val oracleRateXy = 10000L
     val lpBalance = 100000000L
@@ -138,10 +313,6 @@ class LpSpec  extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks
     val deltaReservesY = reservesYOut - reservesYIn
 
     assert(BigInt(deltaReservesX) * reservesYIn * feeDenomLp >= BigInt(deltaReservesY) * (feeNumLp - feeDenomLp) * reservesXIn)
-
-    val dummyInt = 102
-    val dummyToken = "472B4B6250655368566D597133743677397A24432646294A404D635166546A11"
-    val dummyTokenAmount = 110
 
     ergoClient.execute { implicit ctx: BlockchainContext =>
 
@@ -202,6 +373,96 @@ class LpSpec  extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks
 
       // all ok, swap should work
       noException shouldBe thrownBy {
+        TxUtil.createTx(Array(lpBox, dummyBox, fundingBox), Array(oracleBox),
+          Array(validLpOutBox, dummyOutputBox),
+          fee = 1000000L,
+          changeAddress,
+          Array[String](),
+          Array[DhtData](),
+          false
+        )
+      }
+    }
+  }
+
+  property("Swap (sell Dexy) should not work if more Ergs taken") {
+    val lpInCirc = 10000L
+    val oracleRateXy = 10000L
+    val lpBalance = 100000000L
+    val reservesXIn = 1000000000000L
+    val reservesYIn = 100000000L
+
+    val rate = reservesXIn.toDouble / reservesYIn
+    val sellY = 1000L
+    val buyX = (sellY * rate * (feeDenomLp - feeNumLp) / feeDenomLp).toLong  + 1 // take one NanoErg extra
+    assert(buyX == 9970001)
+
+    val reservesXOut = reservesXIn - buyX
+    val reservesYOut = reservesYIn + sellY
+
+    val deltaReservesX = reservesXOut - reservesXIn
+    val deltaReservesY = reservesYOut - reservesYIn
+
+    assert(BigInt(deltaReservesX) * reservesYIn * feeDenomLp < BigInt(deltaReservesY) * (feeNumLp - feeDenomLp) * reservesXIn)
+
+    ergoClient.execute { implicit ctx: BlockchainContext =>
+
+      val fundingBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(fakeNanoErgs)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId1, fakeIndex)
+
+      val dummyBox = // ToDo: see if this can be removed
+        ctx
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(dummyNanoErgs)
+          .tokens(new ErgoToken(dexyUSD, sellY))
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId5, fakeIndex)
+
+      val oracleBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(minStorageRent)
+          .tokens(new ErgoToken(oracleNFT, 1))
+          .registers(KioskLong(oracleRateXy).getErgoValue)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), fakeScript))
+          .build()
+          .convertToInputWith(fakeTxId2, fakeIndex)
+
+      val lpBox =
+        ctx // for funding transactions
+          .newTxBuilder()
+          .outBoxBuilder
+          .value(reservesXIn)
+          .tokens(new ErgoToken(lpNFT, 1), new ErgoToken(lpToken, lpBalance), new ErgoToken(dexyUSD, reservesYIn))
+          .registers(KioskLong(lpInCirc).getErgoValue)
+          .contract(ctx.compileContract(ConstantsBuilder.empty(), lpScript))
+          .build()
+          .convertToInputWith(fakeTxId3, fakeIndex)
+
+      val validLpOutBox = KioskBox(
+        lpAddress,
+        reservesXOut.toLong,
+        registers = Array(KioskLong(lpInCirc)),
+        tokens = Array((lpNFT, 1), (lpToken, lpBalance), (dexyUSD, reservesYOut.toLong))
+      )
+
+      val dummyOutputBox = KioskBox(
+        changeAddress,
+        dummyNanoErgs - deltaReservesX,
+        registers = Array(),
+        tokens = Array()
+      )
+
+      an[Exception] shouldBe thrownBy {
         TxUtil.createTx(Array(lpBox, dummyBox, fundingBox), Array(oracleBox),
           Array(validLpOutBox, dummyOutputBox),
           fee = 1000000L,
